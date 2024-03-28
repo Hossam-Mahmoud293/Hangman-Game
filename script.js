d677b49529d26dde9e9da50dcfbed22c1e39edcf
Hossam Mahmoud <129907915+Hossam-Mahmoud293@users.noreply.github.com>
2024-03-28 13:37:48 +0200

    Update script.js
    Add Background Color To Body

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "E", "F"];
+let colorParts = [];
+
+for (let i = 0; i < 6; i++) {
+    colorParts.push(hexArray[Math.floor(Math.random() * hexArray.length)]);
+}
+
+let finalColor = `#${colorParts.join("")}`;
+
+document.body.style.backgroundColor = finalColor;
+
 const wordList = [
   {
     word: "جيتار",
@@ -379,16 +390,6 @@ changeWordBtn.addEventListener('click', () => {
     getRandomWord();
 });
 
-let hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "E", "F"];
-let colorParts = [];
-
-for (let i = 0; i < 6; i++) {
-    colorParts.push(hexArray[Math.floor(Math.random() * hexArray.length)]);
-}
-
-let finalColor = `#${colorParts.join("")}`;
-
-document.body.style.backgroundColor = finalColor;
 
 getRandomWord();
 playAgainBtn.addEventListener("click", getRandomWord);
