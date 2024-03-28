fa5e0f932f7133f6ec60e0ef8e32c750eed45835
Hossam Mahmoud <129907915+Hossam-Mahmoud293@users.noreply.github.com>
2024-03-28 13:31:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -379,6 +379,16 @@ changeWordBtn.addEventListener('click', () => {
     getRandomWord();
 });
 
+let hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "E", "F"];
+let colorParts = [];
+
+for (let i = 0; i < 6; i++) {
+    colorParts.push(hexArray[Math.floor(Math.random() * hexArray.length)]);
+}
+
+let finalColor = `#${colorParts.join("")}`;
+
+document.body.style.backgroundColor = finalColor;
 
 getRandomWord();
 playAgainBtn.addEventListener("click", getRandomWord);
